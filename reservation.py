e733461868f46fbb35a86f2a68ad19b41bfec46d
Miki Ando <MikiAndo@Mikis-MacBook-Pro.local>
2019-08-23 15:20:19 +0700

    Create reservation.py

---
+++ reservation.py
@@ -0,0 +1,7 @@
+name = str(input("Name: "))
+reservation_name= "Ando"
+
+if name == reservation_name:
+    print "Right this way!"
+else:
+    print "Sorry, we don't have a reservation under that name"
