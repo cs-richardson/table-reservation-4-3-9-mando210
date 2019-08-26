46da06ac25af2cfa1601a1e1a109c4c336664bd2
Miki Ando <MikiAndo@Mikis-MacBook-Pro.local>
2019-08-26 10:51:03 +0700

    reservation

--- reservation.py
+++ reservation.py
@@ -1,3 +1,10 @@
+'''
+This program checks the name that the user inputs with the name that is already
+set in a variable. If the user's name is the same as the reserved name, the
+program will print "Right this way", but if it is not the same, it will print
+"Sorry, we don't have a reservation under that name." 
+'''
+# Miki Ando
 name = str(input("Name: "))
 reservation_name= "Ando"
 
