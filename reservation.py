f5544c9e2f425a233dfaf9c5494338aa979a90ac
Miki Ando <MikiAndo@Mikis-MacBook-Pro.local>
2019-08-28 09:22:38 +0700

    initial commit

--- reservation.py
+++ reservation.py
@@ -4,7 +4,7 @@ set in a variable. If the user's name is the same as the reserved name, the
 program will print "Right this way", but if it is not the same, it will print
 "Sorry, we don't have a reservation under that name." 
 '''
-# Miki Ando
+# Miki Ando 
 name = str(input("Name: "))
 reservation_name= "Ando"
 
